1f5257ec33020ae06882a3425abbf7da207ed5f5
NikolaN <nnovakovic0210@gmail.com>
2023-12-30 11:05:14 +0100

    left_link

--- frontend/src/data/home.js
+++ frontend/src/data/home.js
@@ -46,45 +46,10 @@ export const left = [
       img: "campus",
     },
     {
-      text: "Climate science center",
-      img: "climate",
-    },
-    {
-      text: "Community Help",
-      img: "community",
-    },
-    {
-      text: "COVID-19 Information Centrer",
-      img: "covid",
-    },
-    {
-      text: "Emotional health",
-      img: "emotional",
-    },
-    {
-      text: "Facebook Pay",
-      img: "pay",
-    },
-    {
-      text: "Favorites",
-      img: "fav",
-    },
-    {
-      text: "Fundraisers",
-      img: "fund",
-    },
-    {
-      text: "Gaming Video",
-      img: "gaming",
-    },
-    {
       text: "Jobs",
       img: "jobs",
     },
-    {
-      text: "Live videos",
-      img: "live",
-    },
+    
     {
       text: "Messenger",
       img: "messenger",
@@ -94,84 +59,8 @@ export const left = [
       img: "messkids",
     },
     {
-      text: "Most recent",
-      img: "recent",
-    },
-    {
-      text: "Play Games",
-      img: "play",
-    },
-    {
-      text: "Recent ad activity",
-      img: "recentad",
-    },
-    {
       text: "Weather",
       img: "weather",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
-    },
-    {
-      text: "",
-      img: "",
     },
   ];
   
