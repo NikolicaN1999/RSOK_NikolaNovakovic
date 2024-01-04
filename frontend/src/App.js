95bf29fb64bfcfc995534259f3fa04a40d1e40f9
NikolaN <nnovakovic0210@gmail.com>
2024-01-05 00:48:29 +0100

    emoji

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,27 +4,25 @@ import Profile from "./pages/profile";
 import Home from "./pages/home";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
 import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
+import { useSelector } from "react-redux";
 import Activate from "./pages/home/activate";
 import Reset from "./pages/reset";
 import CreatePostPopup from "./components/createPostPopup";
-import { useSelector } from "react-redux";
 function App() {
-  const { user } = useSelector((state) => ({ ... state}));
+  const { user } = useSelector((state) => ({ ...state }));
   return (
     <div>
-      <CreatePostPopup user={user}/>
+      <CreatePostPopup user={user} />
       <Routes>
-        <Route path="/login" element={<Login />} exact />
         <Route element={<LoggedInRoutes />}>
-        <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
-        <Route path="/activate/:token" element={<Activate />} exact />
+          <Route path="/profile" element={<Profile />} exact />
+          <Route path="/" element={<Home />} exact />
+          <Route path="/activate/:token" element={<Activate />} exact />
         </Route>
         <Route element={<NotLoggedInRoutes />}>
-        <Route path="/login" element={<Login />} exact />
+          <Route path="/login" element={<Login />} exact />
         </Route>
         <Route path="/reset" element={<Reset />} />
-       
       </Routes>
     </div>
   );
