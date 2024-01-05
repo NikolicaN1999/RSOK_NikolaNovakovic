32a77d3f75d28f8fc25b385019016afc0c69c844
NikolaN <nnovakovic0210@gmail.com>
2024-01-05 23:52:39 +0100

    ImagePreview

--- frontend/src/components/createPostPopup/index.js
+++ frontend/src/components/createPostPopup/index.js
@@ -3,25 +3,12 @@ import "./style.css";
 import Picker from "emoji-picker-react";
 import EmojiPickerBackgrounds from "./EmojiPickerBackgrounds";
 import AddToYourPost from "./AddToYourPost";
+import ImagePreview from "./ImagePreview";
 export default function CreatePostPopup({ user }) {
   const [text, setText] = useState("");
-  const [showPrev, setShowPrev] = useState(false);
-  const [picker, setPicker] = useState(false);
-  const textRef = useRef(null);
-  const [cursorPosition, setCursorPosition] = useState();
-  useEffect(() => {
-    textRef.current.selectionEnd = cursorPosition;
-  }, [cursorPosition]);
-  const handleEmoji = ({ emoji }) => {
-    const ref = textRef.current;
-    ref.focus();
-    const start = text.substring(0, ref.selectionStart);
-    const end = text.substring(ref.selectionStart);
-    const newText = start + emoji + end;
-    setText(newText);
-    setCursorPosition(start.length + emoji.length);
-  };
-  console.log(text);
+  const [showPrev, setShowPrev] = useState(true);
+  const [images, setImages] = useState([]);
+
   return (
     <div className="blur">
       <div className="postBox">
@@ -45,26 +32,24 @@ export default function CreatePostPopup({ user }) {
           </div>
         </div>
 
-        {!showPrev && (
+        {!showPrev ? (
           <>
-          <div className="flex_center">
-            <textarea
-              ref={textRef}
-              maxLength="100"
-              value={text}
-              placeholder={`What's on your mind, ${user.first_name}`}
-              className="post_input"
-              onChange={(e) => setText(e.target.value)}
-            ></textarea>
-          </div>
-          <EmojiPickerBackgrounds
+            <EmojiPickerBackgrounds
               text={text}
-              textRef={textRef}
+              user={user}
               setText={setText}
+              showPrev={showPrev}
             />
           </>
+        ) : (
+          <ImagePreview
+            text={text}
+            user={user}
+            setText={setText}
+            showPrev={showPrev}
+          />
         )}
-         <AddToYourPost />
+        <AddToYourPost />
         <button className="post_submit">Post</button>
       </div>
     </div>
