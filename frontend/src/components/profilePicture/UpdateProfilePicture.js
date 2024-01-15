b0fa1d941089e4742490887a7402cde6cbbab79f
NikolicaN1999 <143877364+NikolicaN1999@users.noreply.github.com>
2024-01-15 20:13:04 +0100

    updateProfilePicture

--- frontend/src/components/profilePicture/UpdateProfilePicture.js
+++ frontend/src/components/profilePicture/UpdateProfilePicture.js
@@ -1,7 +1,24 @@
-import { useState } from "react";
-
-export default function UpdateProfilePicture( {setImage} ) {
+import { useCallback, useState} from "react";
+import { useRef } from "react";
+import Crooper from "react-easy-crop";
+export default function UpdateProfilePicture( {image, setImage} ) {
     const [description, setDescription] = useState("");
+    const [crop, setCrop] = useState({ x: 0, y: 0 })
+    const [zoom, setZoom] = useState(1)
+    const slider = useRef(null);
+  
+    const onCropComplete = useCallback ((croppedArea, croppedAreaPixels) => {
+      console.log(croppedArea, croppedAreaPixels)
+    }, []);
+    const zoomIn = () => {
+        slider.current.stepUp();
+        setZoom(slider.current.value);
+    };
+    const zoomOut = () => {
+        slider.current.stepDown();
+        setZoom(slider.current.value);
+    };
+    console.log(zoom);
   return (
     <div className="postBox update_img">
     <div className="box_header">
@@ -18,8 +35,48 @@ export default function UpdateProfilePicture( {setImage} ) {
         className="textarea_blue details_input"></textarea>
     </div>
     <div className="update_center">
-        <div className="crooper"></div>
-    </div>
+        <div className="crooper">
+            <Crooper
+            image = {image}
+            crop = {crop}
+            zoom = {zoom}
+            aspect = { 1 / 1}
+            cropShape="round"
+            onCropChange= {setCrop}
+            onCropComplete = {onCropComplete}
+            onZoomChange = {setZoom}
+            showGrid = {false}
+            />
+        </div>
+        <div className="slider">
+            <div className="slider_circle hover1" onClick={()=> zoomOut()}>
+                <i className="minus_icon"></i>
+            </div>
+            <input type="range" min={1} max={3} step={0.2} ref={slider} value={zoom} onChange={(e)=>setZoom(e.target.value)} />
+            <div className="slider_circle hover1" onClick={()=> zoomIn()}>
+                <i className="plus_icon"></i>
+            </div>
+        </div>
+        </div>
+        <div className="flex_up">
+            <div className="gray_btn">
+                <i className="crop_icon"></i>
+                Crop photo
+            </div>
+            <div className="gray_btn">
+                <i className="temp_icon"></i>
+                Make Temporary
+            </div>
+            
+        </div>
+        <div className="flex_p_t">
+            <i className="public_icon"></i>
+            Your profile picture is public
+        </div>
+        <div className="update_submit_wrap">
+            <div className="blue_link">Cancel</div>
+            <div className="blue_btn">Save</div>
+        </div>
     </div>
   );
 }
--- frontend/src/components/profilePicture/index.js
+++ frontend/src/components/profilePicture/index.js
@@ -53,7 +53,7 @@ export default function ProfilePicture() {
         <div className="old_pictures_wrap"></div>
     </div>
     {
-        image &&  <UpdateProfilePicture setImage={setImage}/>
+        image &&  <UpdateProfilePicture  image={image} setImage={setImage} />
     }
     </div>
   );
