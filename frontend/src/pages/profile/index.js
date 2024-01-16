a4e87bbcc68ef8aba41d8f39ea5050ce8f109cde
NikolicaN1999 <143877364+NikolicaN1999@users.noreply.github.com>
2024-01-16 15:54:17 +0100

    updateProfilePicture

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -17,6 +17,7 @@ export default function Profile({ setVisible }) {
   const { username } = useParams();
   const navigate = useNavigate();
   const { user } = useSelector((state) => ({ ...state }));
+  const [photos, setPhotos] = useState({});
   var userName = username === undefined ? user.username : username;
 
   const [{ loading, error, profile }, dispatch] = useReducer(profileReducer, {
@@ -28,7 +29,9 @@ export default function Profile({ setVisible }) {
     getProfile();
   }, [userName]);
   var visitor = userName === user.username ? false : true;
-  console.log(visitor);
+  const path = `${userName}/*`;
+  const max = 30;
+  const sort = "desc";
 
   const getProfile = async () => {
     try {
@@ -46,6 +49,20 @@ export default function Profile({ setVisible }) {
       if (data.ok === false) {
         navigate("/profile");
       } else {
+        try {
+          const images = await axios.post(
+            `${process.env.REACT_APP_BACKEND_URL}/listImages`,
+            { path, sort, max },
+            {
+              headers: {
+                Authorization: `Bearer ${user.token}`,
+              },
+            }
+          );
+          setPhotos(images.data);
+        } catch (error) {
+          console.log(error);
+        }
         dispatch({
           type: "PROFILE_SUCCESS",
           payload: data,
@@ -58,13 +75,14 @@ export default function Profile({ setVisible }) {
       });
     }
   };
+
   return (
     <div className="profile">
       <Header page="profile" />
       <div className="profile_top">
         <div className="profile_container">
           <Cover cover={profile.cover} visitor={visitor} />
-          <ProfielPictureInfos profile={profile} visitor={visitor} />
+          <ProfielPictureInfos profile={profile} visitor={visitor} photos={photos.resources} />
           <ProfileMenu />
         </div>
       </div>
@@ -74,7 +92,7 @@ export default function Profile({ setVisible }) {
             <PplYouMayKnow />
             <div className="profile_grid">
               <div className="profile_left">
-                <Photos username={userName} token={user.token} />
+                <Photos username={userName} token={user.token} photos={photos}/>
                 <Friends friends={profile.friends} />
                 <div className="relative_fb_copyright">
                   <Link to="/">Privacy </Link>
