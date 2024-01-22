b636f8a61433efb8f8eb665d9df961e47510ed67
NikolicaN1999 <143877364+NikolicaN1999@users.noreply.github.com>
2024-01-22 15:31:07 +0100

    searchMenu

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -20,6 +20,7 @@ const {
   acceptRequest,
   unfriend,
   deleteRequest,
+  search,
 } = require("../controllers/user");
 const { authUser } = require("../middlwares/auth");
 
@@ -44,5 +45,5 @@ router.put("/unfollow/:id", authUser, unfollow);
 router.put("/acceptRequest/:id", authUser, acceptRequest);
 router.put("/unfriend/:id", authUser, unfriend);
 router.put("/deleteRequest/:id", authUser, deleteRequest);
-
+router.post("/search/:searchTerm", authUser, search);
 module.exports = router;
--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -57,7 +57,7 @@ export default function Header({ page, getAllPosts }) {
         </div>
       </div>
       {showSearchMenu && (
-        <SearchMenu color={color} setShowSearchMenu={setShowSearchMenu} />
+        <SearchMenu color={color} setShowSearchMenu={setShowSearchMenu} token={user.token} />
       )}
       <div className="header_middle">
         <Link
