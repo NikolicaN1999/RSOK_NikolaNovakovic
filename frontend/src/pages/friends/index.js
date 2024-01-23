0c5de5099d8250c8fd9f63d79316b1156bab97dc
NikolicaN1999 <143877364+NikolicaN1999@users.noreply.github.com>
2024-01-23 16:27:48 +0100

    friendsPage

--- frontend/src/pages/friends/index.js
+++ frontend/src/pages/friends/index.js
@@ -5,8 +5,12 @@ import { friendspage } from "../../functions/reducers";
 import { getFriendsPageInfos } from "../../functions/user";
 import Card from "./Card";
 import "./style.css";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 export default function Friends() {
   const { user } = useSelector((state) => ({ ...state }));
+  const { type } = useParams();
+  console.log(type);
 
   const [{ loading, error, data }, dispatch] = useReducer(friendspage, {
     loading: false,
@@ -38,13 +42,16 @@ export default function Friends() {
             </div>
           </div>
           <div className="friends_left_wrap">
-            <div className="mmenu_item  active_friends">
-              <div className="small_circle" style={{ background: "#1876f2" }}>
-                <i className="friends_home_icon invert"></i>
+            <Link to="/friends" className={`mmenu_item hover3 ${type === undefined && "active_friends"}`}>
+              <div className="small_circle">
+                <i className="friends_home_icon"></i>
               </div>
               <span>Home</span>
-            </div>
-            <div className="mmenu_item hover3">
+              <div className="rArrow">
+                <i className="right_icon"></i>
+              </div>
+            </Link>
+            <Link to="/friends/requests" className={`mmenu_item hover3 ${type === "requests" && "active_friends"}`}>
               <div className="small_circle">
                 <i className="friends_requests_icon"></i>
               </div>
@@ -52,17 +59,8 @@ export default function Friends() {
               <div className="rArrow">
                 <i className="right_icon"></i>
               </div>
-            </div>
-            <div className="mmenu_item hover3">
-              <div className="small_circle">
-                <i className="friends_suggestions_icon"></i>
-              </div>
-              <span>Suggestions</span>
-              <div className="rArrow">
-                <i className="right_icon"></i>
-              </div>
-            </div>
-            <div className="mmenu_item hover3">
+            </Link>
+            <Link to="/friends/all" className={`mmenu_item hover3 ${type === "all" && "active_friends"}`}>
               <div className="small_circle">
                 <i className="all_friends_icon"></i>
               </div>
@@ -70,14 +68,20 @@ export default function Friends() {
               <div className="rArrow">
                 <i className="right_icon"></i>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
         <div className="friends_right">
-          <div className="friends_right_wrap">
+         {
+          (type === undefined || type === "requests") && (
+            <div className="friends_right_wrap">
             <div className="friends_left_header">
               <h3>Friend Requests</h3>
-              <a className="see_link hover3">See all</a>
+              {
+                type === undefined && (
+                  <Link to="/friends/requests" className="see_link hover3">See all</Link>
+                )
+              }
             </div>
             <div className="flex_wrap">
               {data.requests &&
@@ -86,22 +90,18 @@ export default function Friends() {
                 ))}
             </div>
           </div>
-          <div className="friends_right_wrap">
-            <div className="friends_left_header">
-              <h3>Sent Requests</h3>
-              <a className="see_link hover3">See all</a>
-            </div>
-            <div className="flex_wrap">
-              {data.sentRequests &&
-                data.sentRequests.map((user) => (
-                  <Card userr={user} key={user._id} type="sent" getData={getData}/>
-                ))}
-            </div>
-          </div>
-          <div className="friends_right_wrap">
+          )
+         }
+        {
+           (type === undefined || type === "all") && (
+            <div className="friends_right_wrap">
             <div className="friends_left_header">
               <h3>Friends</h3>
-              <a className="see_link hover3">See all</a>
+              {
+                type === undefined && (
+                  <Link to="/friends/all" className="see_link hover3">See all</Link>
+                )
+              }
             </div>
             <div className="flex_wrap">
               {data.friends &&
@@ -110,6 +110,8 @@ export default function Friends() {
                 ))}
             </div>
           </div>
+           )
+          }
         </div>
       </div>
     </>
